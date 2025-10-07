c09254ec97d148a8cc0346945b1d2c61dbb21443
zerr32 <ludrastar@gmail.com>
2025-10-07 08:41:58 +0700

    first commit

---
+++ tugas ludraizmahendra_19250207.py
@@ -0,0 +1,18 @@
+nama = input("masukan nama pembeli:")
+barang = input("masukan nama barang:")
+harga = int(input("masukan harga sepatu(Rp):"))
+uang = int(input("masukan uang bayar(Rp):"))
+
+kembalian = uang - harga
+
+print("=================================")
+print("          Struk barang           ")
+print("=================================")
+print("Nama pembeli:",nama)
+print("kode barang:",barang)
+print("Harga barang (Rp):",harga)
+print("uang bayar (Rp):",uang)
+print("kembalian(Rp):",kembalian)
+print("=================================")
+print("     Terimakasih telah membeli   ")
+print("=================================")
